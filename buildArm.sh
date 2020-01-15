46b92a7d471473dfee034a174d28d50ce9ffbd15
Asbjørn Enge <asbjorn@hanafjedle.net>
2020-01-15 12:47:02 +0100

    Update the naming scheme a bit

--- buildArm.sh
+++ buildArm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t asbjornenge/tezos-ubuntu-arm:latest -f Dockerfile-ubuntu-arm .
-docker build -t asbjornenge/tezos-ubuntu-arm-zeronet:latest --build-arg net=zeronet -f Dockerfile-ubuntu-arm .
-docker build -t asbjornenge/tezos-ubuntu-arm64:latest -f Dockerfile-ubuntu-arm-aarch64 .
-docker build -t asbjornenge/tezos-ubuntu-arm64-zeronet:latest --build-arg net=zeronet -f Dockerfile-ubuntu-arm-aarch64 .
+docker build -t asbjornenge/tezos-ubuntu-arm:mainnet -f Dockerfile-ubuntu-arm .
+docker build -t asbjornenge/tezos-ubuntu-arm:zeronet --build-arg net=zeronet -f Dockerfile-ubuntu-arm .
+docker build -t asbjornenge/tezos-ubuntu-arm64:mainnet -f Dockerfile-ubuntu-arm-aarch64 .
+docker build -t asbjornenge/tezos-ubuntu-arm64:zeronet --build-arg net=zeronet -f Dockerfile-ubuntu-arm-aarch64 .
